1b07a92f0df9f30e9bf3142e6af4f89226f9d52d
Dheeraj <pilakkatdheeraj@gmail.com>
2026-06-04 18:53:11 +0530

    feat: redesign landing page with new header, footer, and city highlight sections

---
+++ app/components/ui/Header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="flex items-center justify-between px-4 mt-4 mx-16 py-2 bg-neutral-200 rounded-lg">
+      <div className='flex gap-x-4 items-center'>
+        <Image src="/logo.png" alt="logo" width={50} height={50} className='rounded-full size-12' />
+        <h1 className="text-3xl font-black text-primary">Nammude Kozhikode</h1>
+      </div>
+        <div className='text-2xl py-3'>
+            <button className="px-1 hover:font-bold hover:tracking-wider py-1 mx-3 hover:text-primary hover:underline transition-all  delay-10 duration-300 ease-in-out rounded-none border-primary active:text-primary">Features</button>
+            <button className="px-1 hover:font-bold hover:tracking-wider py-1 mx-3 hover:text-primary hover:underline transition-all  delay-10 duration-300 ease-in-out rounded-none border-primary active:text-primary">Officials</button>
+            <button className="px-1 hover:font-bold hover:tracking-wider py-1 mx-3 hover:text-primary hover:underline transition-all  delay-10 duration-300 ease-in-out rounded-none border-primary active:text-primary">How it works</button>
+        </div>
+        <div className='bg-primary text-neutral p-3 rounded-full '>
+            <h1>Report issue</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,67 @@
 import Image from "next/image";
+import { FaBullhorn } from "react-icons/fa";
+import { MdReportGmailerrorred } from "react-icons/md";
+import { LuHandHelping } from "react-icons/lu";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="mx-16 my-4 min-h-screen">
+      <div className="flex h-162.5 w-full gap-4 @container flex-col lg:flex-row">
+        <div className="relative h-162.5 w-3/4">
+          <Image src="/beach1.webp" alt="logo" width={1100} height={300} className="h-[650px] w-full rounded-lg"/>
+          <div className="absolute bottom-20 gap-y-5 flex flex-col left-3">
+            <h1 className="text-neutral text-5xl font-bold">Build a Smarter </h1> <h1 className="text-neutral text-5xl font-bold"> Kozhikode, Together</h1>
+            <p className="text-neutral-500 text-xl ">Report issues, track resolutions, and empower your neighborhood through collective action.</p>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-col bg-gray-200 gap-y-4 border-t-primary border-t-4 h-full w-1/4 p-9 rounded-lg  items-start justify-evenly px-4">
+        <div className="flex items-center gap-x-4">
+          <div className="bg-primary/10 p-3 rounded-2xl">
+            <FaBullhorn className="text-2xl text-primary"/>
+          </div>
+        <h1 className="text-2xl font-semibold">Have a concern in your ward?</h1>
         </div>
-      </main>
+        <p className="text-neutral-500 text-xl">Your voice matters. Report infrastructure issues directly to the Corporation in under 60 seconds.</p>
+        <button className="bg-primary text-white px-4 py-4 rounded-lg flex items-center justify-center gap-x-2 text-xl w-full">
+          <MdReportGmailerrorred />
+          Report an Issue
+        </button>
+        </div>
+      </div>
+      <div className="@container">
+        <div className="flex flex-col justify-center gap-y-4 my-16">
+          <h1 className="text-3xl font-bold ">City Highlights</h1>
+          <p className="text-xl font-light text-neutral-400">Discover the rich culture and vibrant life of Kozhikode.</p>
+        </div>
+        <div className="flex justify-evenly  ">
+          <div className="bg-neutral-200 h-120 w-96  border-neutral-400 border-2 p-4 rounded flex flex-col gap-y-6 ">
+            <div>
+              <Image src="/chicken-biriyani.webp" alt="logo" width={300} height={500} className="h-40 w-full rounded"/>
+            </div>
+            <h2 className="text-xl font-bold">Culinary Heritage</h2>
+            <p>The Food Capital of Kerala, famous for its mouth-watering Halwa and iconic Biryani. A melting pot of flavors shaped by centuries of trade.</p>
+          </div>
+          <div className="bg-neutral-200 h-120 w-96 border-neutral-400 border-2 rounded">
+            <h2 className="text-xl font-bold">Cultural Events</h2>
+            <p>Join us for exciting cultural celebrations.</p>
+          </div>
+          <div className="bg-neutral-200 h-120 w-96 border-neutral-400 border-2 rounded">
+            <h2 className="text-xl font-bold">Local Cuisine</h2>
+            <p>Delight in the flavors of our region.</p>
+          </div>
+        </div>
+      </div>
+      <div className="@container flex items-center justify-center ">
+        <div className="flex flex-col justify-center gap-y-4 my-16 w-1/2 bg-neutral-200 items-center text-center border-2 border-neutral-400 p-8 rounded-lg py-12">
+        <div className="bg-blue-950/20 p-3 rounded-lg"><LuHandHelping className="text-primary text-2xl" /></div>
+          <h1 className="text-3xl font-bold ">Preserving our Pride</h1>
+          <p className="text-xl font-medium text-neutral-500">Help us maintain the beauty of our historic city. Report issues like broken pavements or faulty streetlights to keep Our Kozhikode clean, safe, and welcoming.</p>
+          <button className="bg-primary text-white px-4 py-4 rounded-lg flex items-center justify-center gap-x-2 text-xl w-fit">
+          <MdReportGmailerrorred />
+          Report an Issue
+        </button>
+        </div>
+      </div>
     </div>
   );
 }
